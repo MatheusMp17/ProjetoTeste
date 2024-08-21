7ab4fcc8915950504bbe198598097c2be090e9f9
Matheus M Pedroso <matheusmorschbacher.mmp@gmail.com>
2024-08-21 14:57:15 -0300

    Segunda versão

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ import java.util.*;
 public class Main{
     public static void main(String[] args) {
         System.out.println("teste");
+        System.out.println("Fiz outras coisas...");
     }
 }
